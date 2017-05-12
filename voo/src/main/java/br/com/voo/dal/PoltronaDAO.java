f2f04569e9d8773750424ead94164372fdab38db
Bruno Rodrigues <brunocroh@gmail.com>
2017-05-11 22:30:35 -0300

    testes de poltrona

--- voo/src/main/java/br/com/voo/dal/PoltronaDAO.java
+++ voo/src/main/java/br/com/voo/dal/PoltronaDAO.java
@@ -1,5 +1,40 @@
 package br.com.voo.dal;
 
-public class PoltronaDAO {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
+import br.com.voo.model.Poltrona;
+import br.com.voo.util.FactoryConexao;
+
+public class PoltronaDAO {
+	
+	final static Logger log = Logger.getLogger(PoltronaDAO.class.getName());
+
+	private Connection conexao;
+
+    public PoltronaDAO() {
+        conexao = FactoryConexao.getConnection();
+    }
+    
+    public boolean salvar() throws SQLException{
+    	return true;
+    }
+    
+    public boolean consultar() throws SQLException{
+    	return true;
+    }
+
+	public boolean alterar() throws SQLException{
+
+		return true;
+	}
+
+	public boolean excluir(Poltrona poltrona)throws SQLException {
+		
+		return true;
+	}
+    
+    
+	
 }
--- voo/src/main/java/br/com/voo/model/Poltrona.java
+++ voo/src/main/java/br/com/voo/model/Poltrona.java
@@ -1,5 +1,45 @@
 package br.com.voo.model;
 
-public class Poltrona extends Aeronave {
+public class Poltrona extends Entidade {
+	
+	private String descricao;
+	private Double valor;
+	private String classe;
+	private String detalhes;
 
+	public Poltrona() {
+		super();
+	}
+
+	public Poltrona(String descricao, Double valor, String classe, String detalhes) {
+		this();
+		this.descricao = descricao;
+		this.valor = ifValorMenorQueZero(valor);
+		this.classe = classe;
+		this.detalhes = detalhes;
+	}
+	
+	public Poltrona(Long id,String descricao, Double valor, String classe, String detalhes) {
+		this(descricao,valor,classe,detalhes);
+		this.id = ifValorMenorQueZero(id);
+	}
+	
+	
+	private double ifValorMenorQueZero(Double valor) {
+		if(valor > 0) {
+			return new Double(valor);
+		}else {
+			return new Double(0);
+		}
+	}
+	
+	private Long ifValorMenorQueZero(Long valor) {
+		if(valor > 0) {
+			return new Long(valor);
+		}else {
+			return new Long(0);
+		}
+	}
+
+	
 }
