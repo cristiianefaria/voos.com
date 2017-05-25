323521a7e343b7fa1235f78925ac5902b59d0c3e
cristiianefaria <cristiane.trc@gmail.com>
2017-05-24 21:50:49 -0300

    criação classe entidade voo, e classe bs

--- voo/src/main/java/br/com/voo/dal/VooDAO.java
+++ voo/src/main/java/br/com/voo/dal/VooDAO.java
@@ -1,5 +1,51 @@
 package br.com.voo.dal;
 
-public class VooDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import br.com.voo.model.Aeronave;
+import br.com.voo.model.Voo;
+import br.com.voo.util.FactoryConexao;
+
+public class VooDAO {
+	
+	VooDAO dao;
+	Connection cnn;
+	
+	public VooDAO(){
+		this.dao = new VooDAO();
+		cnn = FactoryConexao.getConnection();
+	}
+	
+	public boolean incluir(Voo voo) throws SQLException{
+		PreparedStatement ps = cnn.prepareStatement("INSERT INTO voo ");
+		return true;
+	}
+	
+	public boolean alterar(Voo voo) throws SQLException{
+		PreparedStatement ps = cnn.prepareStatement("INSERT INTO voo ");
+		return true;
+	}
+	
+	public List<Voo> listar() throws SQLException {
+		return new ArrayList<>();
+	}
+	
+	public boolean remover(Long id) throws SQLException {
+		PreparedStatement ps = cnn.prepareStatement("delete from voo where codigo=?");
+		ps.setLong(1, id);
+		ps.executeQuery();
+		
+		ps.close();
+		
+		return true;
+	}
+
+	public Voo consultar(Voo voo) throws SQLException {
+		return new Voo();
+	}
+	
 }
