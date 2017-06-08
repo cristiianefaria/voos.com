4bb7e67631ae1b82044eb1076f035fec4e822330
Bruno Rodrigues <brunocroh@gmail.com>
2017-06-08 20:36:06 -0300

    banco mapeado

---
+++ voo/src/main/webapp/app/js/passageiro.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+    $("#horario").datetimepicker({
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-arrow-up",
+            down: "fa fa-arrow-down",
+            previous: 'fa fa-angle-left',
+            next: 'fa fa-angle-right'
+        },
+        locale: 'pt-br'
+    });	
+	
+})
