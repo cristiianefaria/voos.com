8e043a25debeca363084594e0c1be0cb2b9afa55
Thiago Rodrigues <thiagorodriguescamara@gmail.com>
2017-06-08 21:32:57 -0300

    Atualizando Pasta de Testes

---
+++ voo/src/test/java/br/com/voo/bll/PassagemBSTest.java
@@ -0,0 +1,13 @@
+package br.com.voo.bll;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PassagemBSTest {
+
+	@Test
+	public void test() {
+	}
+
+}
---
+++ voo/src/test/java/br/com/voo/bll/VooBSTest.java
@@ -0,0 +1,13 @@
+package br.com.voo.bll;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VooBSTest {
+
+	@Test
+	public void test() {
+	}
+
+}
